53f98588b0e0bc652cb835a273c7490497826267
igorifaresi <igor.ifaresi@gmail.com>
2020-05-07 17:09:56 -0300

    Initial commit

---
+++ controller.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const service = require('./service.js');
+const bodyParser = require('body-parser');
+
+service.initServer();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/institution/create', (req, res) => {
+    if (req.body.login != null) {
+        service.checkLogin(req.body.login).then((exists) => {
+            if (!exists) {
+                service.createInstitution(req.body).then((s) => {
+                    if (s) {
+                        res.send('New institution created with success');
+                    } else {
+                        res.status(400).send('Invalid instituion data');
+                    }
+                });
+            } else {
+                res.status(400).send('This login already exists');
+            }
+        });        
+    } else {
+        res.status(400).send('Login required');
+    }    
+});
+
+app.post('/institution/update', (req, res) => {
+    if (req.body.login != null
+    &&  req.body.token != null) {
+        service.checkCredentials(req.body.login, req.body.token).then((s) => {
+            if (s) {
+                service.updateInstitutionData(
+                    req.body.login,
+                    req.body.token,
+                    req.body.obj
+                ).then((s) => {
+                    if (s) {
+                        res.send('Institution updated with success');
+                    } else {
+                        res.status(400).send('Error in institution update');
+                    }
+                });
+            } else {
+                res.status(400).send('Invalid credentials');
+            }
+        });
+    } else {
+        res.status(400).send('Credentials required');
+    }
+});
+
+app.post('/institution/query', (req, res) => {
+    service.queryInstitutions(req.body).then((result) => {
+        res.send(result);
+    });
+});
+
+app.post('/institution/get', (req, res) => {
+    if (req.body.number != null && typeof req.body.number === 'number') {
+        service.getInstitutionByNumber(req.body.number).then((result) => {
+            res.send(result);
+        });
+    } else {
+        res.status(400).send('Institution number required');
+    }
+});
+
+//start listening the server
+app.listen(3000);
---
+++ service.js
@@ -0,0 +1,160 @@
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+/*
+ * MongoDB models
+ */
+const InstitutionModel = mongoose.model('InstitutionModel', new mongoose.Schema({
+        login: {
+                type: String,
+                require: true,
+        },
+        token: {
+                type: String,
+                require: true,
+        },
+        num: {
+                type: Number,
+                require: true,
+        },
+        name: { 
+                type: String,
+                require: true,
+        },
+        description: {
+                type: String,
+                require: true,
+        },
+        necessities: {
+                type: String,
+                require: true,
+        },
+        created: {
+                type: Date,
+                require: Date.now,
+        },
+        lastPush: Date,
+        instantNecessities: {
+                type: [Boolean],
+                require: true
+        },
+        /*
+        phone: String,
+        latitude: Number,
+        longitude: Number,
+        */
+        //TODO:
+        //phone, localization, coordinates, email
+}));
+
+//TODO: type checking
+
+module.exports.initServer = function() {
+        let init = (new Date()).getTime();
+        mongoose.connect(process.env.DB_CONNECTION, { 
+                useNewUrlParser    : true,
+                useUnifiedTopology : true,
+                useFindAndModify   : false,
+        }, () => {
+                console.log('Connected to server! in '
+                +((new Date()).getTime() - init)/1000+' secs');
+        });
+}
+
+module.exports.checkLogin = async function(login) {
+        let query = InstitutionModel.findOne({ 
+                "login" : login 
+        });
+        return ((await query) != null);
+}
+
+module.exports.checkCredentials = async function(login, token) {
+        let query = InstitutionModel.findOne({ 
+                "login" : login,
+                "token" : token,
+        });
+        return ((await query) != null);
+}
+
+module.exports.createInstitution = async function(obj) {
+        if (obj.login != null
+        &&  obj.token != null 
+        &&  obj.name  != null
+        &&  obj.description != null
+        &&  obj.necessities != null
+        &&  obj.instantNecessities != null) {
+                let n = {};
+                n.login = obj.login;
+                n.token = obj.token;
+                n.name = obj.name;
+                n.description = obj.description;
+                n.necessities = obj.necessities;
+                n.instantNecessities = obj.instantNecessities;
+
+                n.num = (await InstitutionModel.count());
+                n.created = Date.now();
+
+                let s = true;
+                await new InstitutionModel(n).save((err) => {
+                        if (err) {
+                                console.log(err);
+                                s = false; 
+                        }
+                });
+                return s;
+        } else {
+                return false;
+        }
+}
+
+module.exports.updateInstitutionData = async function(login, token, obj) {
+        let tmp = {};
+        if (obj.name != null)
+                tmp.name = obj.name;
+        if (obj.description != null)
+                tmp.description = obj.description;
+        if (obj.necessities != null)
+                tmp.necessities = obj.necessities;
+        if (obj.instantNecessities != null)
+                tmp.instantNecessities = obj.instantNecessities;
+
+        let status = true;
+        await InstitutionModel.findOneAndUpdate({
+                "login" : login,
+                "token" : token,
+        }, tmp, (err, item, res) => {
+                if (err) {
+                        console.log(err);
+                        status = false;
+                }
+        });
+        return status;
+}
+
+module.exports.queryInstitutions = async function(obj) {
+        let query = {};
+        if (obj.name != null) {
+                query.name = {
+                        $regex: new RegExp(obj.name),
+                        $options: 'i',
+                };
+        }
+        if (obj.description != null) {
+                query.description = {
+                        $regex: new RegExp(obj.description), 
+                        $options: 'i',
+                };
+        }
+        if (obj.necessities != null) {
+                query.necessities = {
+                        $regex: new RegExp(obj.description), 
+                        $options: 'i',
+                };
+        }
+        //TODO add instant necessities search and coordinates and other params
+        return (await InstitutionModel.find(query));
+}
+
+module.exports.getInstitutionByNumber = async function(num) {
+        return (await InstitutionModel.findOne({ "num" : num }));
+}
